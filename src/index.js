52a291c475b3c582c79deb068bf6758140a9b68f
Alexey Winter <lexawin@gmail.com>
2022-05-07 14:07:51 +0300

    Made getting the city from the object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { render } from 'react-dom';
+import MainService from './services/MainService';
 
-const element = <h1>Weather Today in Kaluga</h1>;
+const mainService = new MainService();
+const weather = mainService.getWeather();
+const city = weather.city;
+
+const element = <h1>Weather Today in {city}</h1>;
 const container = document.querySelector('.app');
 
 render(element, container);
