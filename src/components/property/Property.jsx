84cd68a316b2638f1d35add436e4b57244a595f0
Alexey Winter <lexawin@gmail.com>
2022-05-10 22:35:03 +0300

    Added icons for weather properties

--- src/components/property/Property.jsx
+++ src/components/property/Property.jsx
@@ -3,8 +3,19 @@ import './Property.css';
 function Property(props) {
   const { name, value, unit } = props;
 
+  const classNames = {
+    cloudcover: 'property__cloudcover',
+    humidity: 'property__humidity',
+    'wind speed': 'property__wind-speed',
+    pressure: 'property__pressure',
+    'uv index': 'property__uv-index',
+    visibility: 'property__visibility',
+  };
+
+  const className = `property ${classNames[name]}`;
+
   return (
-    <li className="property">
+    <li className={className}>
       <h3 className="property-name">{name}</h3>
       <p className="property-value">{value + unit}</p>
     </li>
