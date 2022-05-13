04596a8780b4e0e36b75969613ea4b719f7c7bee
Alexey Winter <lexawin@gmail.com>
2022-05-13 17:04:25 +0300

    Converted a functional component App to a class
    Add state and lifecycle method componentDidMount

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,32 +1,62 @@
+import { Component } from 'react';
 import MainService from '../../services/MainService';
 import Properties from '../properties/Properties';
 import WeatherDescription from '../weatherDescription/WeatherDescription';
 
 import './App.css';
 
-function App() {
-  const mainService = new MainService();
-  const weather = mainService.getWeather();
-  const { city, temperature, weatherDescription, weatherProperties, isDay } =
-    weather;
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  let mainClassName = 'main';
-  if (isDay) mainClassName += ' main__day';
+    this.mainService = new MainService();
 
-  return (
-    <main className={mainClassName}>
-      <h1>
-        Weather Now in
-        <br />
-        <span className="city-name">{city}</span>
-      </h1>
-      <div className="main-properties">
-        <WeatherDescription description={weatherDescription} />
-        <p className="temperature">{temperature}°</p>
-      </div>
-      <Properties properties={weatherProperties} />
-    </main>
-  );
+    this.state = {
+      weather: {
+        city: undefined,
+        temperature: undefined,
+        weatherDescription: 'undefined',
+        weatherProperties: {
+          cloudcover: undefined,
+          humidity: undefined,
+          windSpeed: undefined,
+          pressure: undefined,
+          uvIndex: undefined,
+          visibility: undefined,
+        },
+        isDay: undefined,
+      },
+    };
+  }
+
+  componentDidMount() {
+    const weather = this.mainService.getWeather();
+
+    this.setState({ weather });
+  }
+
+  render() {
+    const { city, temperature, weatherDescription, weatherProperties, isDay } =
+      this.state.weather;
+
+    let mainClassName = 'main';
+    if (isDay) mainClassName += ' main__day';
+
+    return (
+      <main className={mainClassName}>
+        <h1>
+          Weather Now in
+          <br />
+          <span className="city-name">{city}</span>
+        </h1>
+        <div className="main-properties">
+          <WeatherDescription description={weatherDescription} />
+          <p className="temperature">{temperature}°</p>
+        </div>
+        <Properties properties={weatherProperties} />
+      </main>
+    );
+  }
 }
 
 export default App;
