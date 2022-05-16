e1a349d72606fbd92ccbf4c9ce95679624c76a21
Alexey Winter <lexawin@gmail.com>
2022-05-16 06:56:35 +0300

    Extracted Main component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,75 +1,11 @@
 import { Component } from 'react';
-import MainService from '../../services/MainService';
-import Properties from '../properties/Properties';
-import WeatherDescription from '../weatherDescription/WeatherDescription';
+import Main from '../main/Main';
 
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.mainService = new MainService();
-
-    this.state = {
-      weather: {
-        city: undefined,
-        temperature: undefined,
-        weatherDescription: 'undefined',
-        weatherProperties: {
-          cloudcover: undefined,
-          humidity: undefined,
-          windSpeed: undefined,
-          pressure: undefined,
-          uvIndex: undefined,
-          visibility: undefined,
-        },
-        isDay: undefined,
-      },
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidMount() {
-    const weather = this.mainService.getWeather();
-
-    this.setState({ weather });
-  }
-
-  handleClick() {
-    this.setState(({ weather: { city: prevCity } }) => {
-      const newCity = prevCity === 'Los Angeles' ? 'Kaluga' : 'Los Angeles';
-
-      const weather = this.mainService.getWeather(newCity);
-
-      return { weather };
-    });
-  }
-
   render() {
-    const { city, temperature, weatherDescription, weatherProperties, isDay } =
-      this.state.weather;
-
-    let mainClassName = 'main';
-    if (isDay) mainClassName += ' main__day';
-
-    return (
-      <main className={mainClassName}>
-        <h1>
-          Weather Now in
-          <br />
-          <span className="city-name" onClick={this.handleClick}>
-            {city}
-          </span>
-        </h1>
-        <div className="main-properties">
-          <WeatherDescription description={weatherDescription} />
-          <p className="temperature">{temperature}°</p>
-        </div>
-        <Properties properties={weatherProperties} />
-      </main>
-    );
+    return <Main />;
   }
 }
 
