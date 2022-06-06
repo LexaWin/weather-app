0d4d3f0a72bfb31b3461e908f9b83188f841e2f4
Alexey Winter <lexawin@gmail.com>
2022-06-06 13:52:01 +0300

    Rewrote the CityInput component on hooks

--- src/components/cityInput/CityInput.jsx
+++ src/components/cityInput/CityInput.jsx
@@ -1,60 +1,47 @@
-import { Component, createRef } from 'react';
+import { useState, useEffect, createRef } from 'react';
 
 import './CityInput.css';
 
 import btn from '../../img/btn-close.svg';
 
-class CitySelect extends Component {
-  constructor(props) {
-    super(props);
+function CitySelect(props) {
+  const [value, setValue] = useState('');
 
-    this.state = {
-      value: '',
-    };
+  const input = createRef();
 
-    this.input = createRef();
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  function handleChange(e) {
+    setValue(e.target.value);
   }
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
-
-  handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    this.props.handleInput(this.state.value);
+    props.handleInput(value);
   }
 
-  componentDidMount() {
-    this.input.current.focus();
-  }
+  useEffect(() => input.current.focus(), []);
 
-  render() {
-    return (
-      <div className="city-input">
-        <button className="btn-close" onClick={this.props.handleClose}>
-          <img src={btn} alt="Close" width="25" />
+  return (
+    <div className="city-input">
+      <button className="btn-close" onClick={props.handleClose}>
+        <img src={btn} alt="Close" width="25" />
+      </button>
+
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          ref={input}
+          type="text"
+          value={value}
+          onChange={handleChange}
+          className="form-input"
+          placeholder="Enter your city here..."
+          required
+        />
+        <button type="submit" className="form-submit">
+          OK
         </button>
-
-        <form className="form" onSubmit={this.handleSubmit}>
-          <input
-            ref={this.input}
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-            className="form-input"
-            placeholder="Enter your city here..."
-            required
-          />
-          <button type="submit" className="form-submit">
-            OK
-          </button>
-        </form>
-      </div>
-    );
-  }
+      </form>
+    </div>
+  );
 }
 
 export default CitySelect;
